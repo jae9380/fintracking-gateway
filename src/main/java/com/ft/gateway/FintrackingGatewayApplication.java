146e4d9182372f71583dc1dbb0f5e7d6481189b0
jae9380 <ljy5314@gmail.com>
2026-04-16 19:28:27 +0900

    Fintracking GateWay Server

---
+++ src/main/java/com/ft/gateway/FintrackingGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ft.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrackingGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintrackingGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ft/gateway/FintrackingGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ft.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackingGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
